cfd93d3c8d8752c6721ee5eaf24bf2824f16fc40
Xisney <44728715+Xisney@users.noreply.github.com>
2019-06-08 16:41:00 +0800

    Create heartdraw.py
    一个利用python的turtle库绘制爱心的函数

---
+++ heartdraw.py
@@ -0,0 +1,28 @@
+"""
+
+version:0.1
+author:xxx
+date: 2019-06-08
+"""
+from turtle import *
+
+
+def main():
+    def curvemove():
+        for i in range(200):
+            right(1)
+            forward(1)
+    color('violet', 'pink')
+    begin_fill()
+    left(140)
+    forward(111.65)
+    curvemove()
+    left(120)
+    curvemove()
+    forward(111.65)
+    end_fill()
+    done()
+
+
+if __name__ == '__main__':
+    main()
