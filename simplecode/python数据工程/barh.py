18181f3ce9b017e2e0b7d38f66ae1c1952e56dd4
Xisney <44728715+Xisney@users.noreply.github.com>
2019-10-02 13:44:05 +0800

    update new demo
    利用matplotlib绘制简单的条形图

---
+++ simplecode/python数据工程/barh.py
@@ -0,0 +1,25 @@
+"""
+python数据分析
+使用matplotlib绘图
+数据链接 = https://github.com/TheisTrue/DataAnalysis
+author: xys
+version: 2.0
+date: 2019-10-02
+"""
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+
+font = font_manager.FontProperties(fname='./fonts/华文楷体.ttf')
+a = ["战狼2", "速度与激情8", "功夫瑜伽", "西游伏妖篇", "变形金刚5：最后的骑士", "摔跤吧！爸爸", "加勒比海盗5：死无对证", "金刚：骷髅岛", "极限特工：终极回归", "生化危机6：终章",
+     "乘风破浪", "神偷奶爸3", "智取威虎山", "大闹天竺", "金刚狼3：殊死一战", "蜘蛛侠：英雄归来", "悟空传", "银河护卫队2", "情圣", "新木乃伊", ]
+b = [56.01, 26.94, 17.53, 16.49, 15.45, 12.96, 11.8, 11.61, 11.28, 11.12, 10.49, 10.3, 8.75, 7.55, 7.32, 6.99, 6.88,
+     6.86, 6.58, 6.23]
+# x = list(range(len(a)))
+plt.figure(figsize=(19.2, 10.8), dpi=200)
+plt.barh(range(len(a)), b, height=0.3, color='violet')
+plt.yticks(range(len(a)), a, fontproperties=font)
+plt.ylabel('电影名', fontproperties=font)
+plt.xlabel('票房(亿)', fontproperties=font)
+plt.title('票房展示', fontproperties=font)
+plt.grid(alpha=0.3)
+plt.show()
